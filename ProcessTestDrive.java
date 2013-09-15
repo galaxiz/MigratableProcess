7a3d47239d650b07f9219c7ace2f71e93c8965ed
Xi Zhao <zx1452@hotmail.com>
2013-09-15 16:54:12 -0400

    Add Serialization Test to TestDrive

--- ProcessTestDrive.java
+++ ProcessTestDrive.java
@@ -1,3 +1,9 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
 /**
  * 
  */
@@ -9,7 +15,7 @@
 public class ProcessTestDrive {
 
 	public void testGrep() {
-		String test[] = { "hello", "in.txt", "out.txt" };
+		String test[] = { "gig", "in.txt", "out.txt" };
 		try {
 			GrepProcess gp = new GrepProcess(test);
 			Thread testdrive = new Thread((Runnable) gp);
@@ -19,8 +25,16 @@ public class ProcessTestDrive {
 			while (i < 10) {
 				gp.suspend();
 				Thread.sleep(500);
-				// testdrive.stop();
-				Thread testdrive2 = new Thread((Runnable) gp);
+				
+				FileOutputStream fout=new FileOutputStream("serial");
+				ObjectOutputStream out=new ObjectOutputStream(fout);
+				out.writeObject(gp);
+				
+				FileInputStream fin=new FileInputStream("serial");
+				ObjectInputStream in=new ObjectInputStream(fin);
+				MigratableProcess job=(MigratableProcess)in.readObject();
+				
+				Thread testdrive2 = new Thread((Runnable) job);
 				testdrive2.start();
 				Thread.sleep(500);
 				i++;
@@ -55,6 +69,37 @@ public class ProcessTestDrive {
 		}
 
 	}
+	
+	public void testSerializable() {
+		String test[] = { "gig", "in.txt", "out.txt" };
+		try {
+			MigratableProcess job=new TestSerializableJob();
+			Thread testdrive = new Thread((Runnable) job);
+			testdrive.start();
+			Thread.sleep(500);
+			int i = 0;
+			while (i < 10) {
+				job.suspend();
+				Thread.sleep(500);
+				
+				FileOutputStream fout=new FileOutputStream("serial");
+				ObjectOutputStream out=new ObjectOutputStream(fout);
+				out.writeObject(job);
+				
+				FileInputStream fin=new FileInputStream("serial");
+				ObjectInputStream in=new ObjectInputStream(fin);
+				job=(MigratableProcess)in.readObject();
+				
+				Thread testdrive2 = new Thread((Runnable) job);
+				testdrive2.start();
+				Thread.sleep(500);
+				i++;
+			}
+		} catch (Exception e) {
+			System.out.println("Exception " + e);
+			e.printStackTrace();
+		}
+	}
 
 	/**
 	 * Sep 12, 2013
@@ -63,8 +108,9 @@ public class ProcessTestDrive {
 	 */
 	public static void main(String[] args) {
 		ProcessTestDrive ptd = new ProcessTestDrive();
-//		ptd.testGrep();
-		ptd.testZip();
+		ptd.testGrep();
+		//ptd.testZip();
+		//ptd.testSerializable();
 	}
 
 }
